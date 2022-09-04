ee9c37102948e51d8eb29468c4c08e1b43e74cc2
Evan van der Voort <evanvandervoort@gmail.com>
2022-09-04 16:05:36 -0400

    Add interpolation file names

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import re
 from matplotlib.pyplot import figure, show
 
 import merger as mg
@@ -37,20 +39,73 @@ def read_data(m1, m2):
     return data
 
 
+def mass_ranges():
+    """ Determine which masses are in the database """
 
-def interp_value(m, mass="Primary"):
+        # Path to data files 
+    path1 = "//Users//Evan//Documents//Evan//Studie//BSc//"
+    path2 = "Honours College//grav-wave-modeller//merger_data"
+    
+    fileList = os.listdir(path1 + path2)    # List of files in database
+
+    primList, secList = [], []              # Lists to store data in
+
+    for file in fileList:                   # Looping over file names
+
+            # Cleaning string
+        newName = (file.replace('merger', '')).replace('.csv', '')
+        undInd = newName.rfind('_')         # Position of underscore
+
+        primMass = int(newName[:undInd])    # Primary mass value
+        secMass = int(newName[undInd+1:])   # Secondary mass value
+
+        primList.append(primMass)
+        secList.append(secMass)
+    
+    return np.unique(primList), np.unique(secList)
+    
+def closest_ind(m, mList, ind):
+    """ Find two closest indices for m in mList """
+
+    if mList[ind] > m: return [ind-1, ind]
+    elif mList[ind] < m: return [ind, ind+1]
+    else: return [ind]                                 # No interpolation needed
+
+
+def find_inds(m1, m2):
     """ Interpolate data for primary or secondary mass"""
 
-    if mass == "Primary": listMass = np.array([15,20])      # Primary mass
-    elif mass == "Secondary": listMass = np.array([15,20])  # Secondary mass
+    primMass, secMass = mass_ranges()           # Prim and sec masses in database
 
-    secInd = (np.abs(listMass - m)).argmin()            # Index for secondary mass
+    primInd = (np.abs(primMass - m1)).argmin()  # Index for primary mass
+    secInd = (np.abs(secMass - m2)).argmin()    # Index for secondary mass
 
-        # Finding second closest index
-    if listMass[secInd] < m: return secInd-1, secInd
-    elif listMass[secInd] > m: return secInd, secInd+1
-    else: return secInd                                 # No interpolation needed
+        # Finding two closest indices for primary & secondary mass
+    return closest_ind(m1, primMass, primInd), closest_ind(m2, secMass, secInd)
 
+
+def interp_data(m1, m2):
+    """ Interpolate data """
+
+    if m1 < m2:
+        raise ValueError("Primary mass must be bigger than secondary mass")
+
+    primMass, secMass = mass_ranges()       # Prim and sec masses in database
+    primInd, secInd = find_inds(m1, m2)     # Find indices of closest values
+
+    fNames = []
+
+        # Retrieving file names
+    for mP in primMass[primInd]:        # Looping over primary masses
+
+        fName = "merger" + str(mP)      # Primary mass
+
+        for mS in secMass[secInd]:      # Looping over secondary masses
+
+            fSec = fName + "_" + str(mS) + ".csv"
+            fNames.append(fSec)
+    
+    return fNames
 
 
 
@@ -77,6 +132,4 @@ def plot_data(m1, m2, saveFig=None):
     if saveFig: fig.savefig(saveFig)
     show()
     
-
-save_data(20, 15)
-plot_data(20, 15)
+print(interp_data(18, 16))
